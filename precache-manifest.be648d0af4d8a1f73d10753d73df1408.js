900e99c2c8249c63cf0b26cc0cf6ce42f4feaa05
Olaniyan Foluso <olaniyanfoluso498@gmail.com>
2020-08-28 03:04:44 +0100

    Updates

---
+++ precache-manifest.be648d0af4d8a1f73d10753d73df1408.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2dad07071419d562af6ce62d97f501",
+    "url": "/recipee/index.html"
+  },
+  {
+    "revision": "2108e330193f9d8fa873",
+    "url": "/recipee/static/css/main.5bc20da9.chunk.css"
+  },
+  {
+    "revision": "f2ad899d48ba7858ee3c",
+    "url": "/recipee/static/js/2.80d14b9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipee/static/js/2.80d14b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2108e330193f9d8fa873",
+    "url": "/recipee/static/js/main.9873077a.chunk.js"
+  },
+  {
+    "revision": "ad6cf1bfb44e1354af3a",
+    "url": "/recipee/static/js/runtime-main.8d64dcee.js"
+  }
+]);
